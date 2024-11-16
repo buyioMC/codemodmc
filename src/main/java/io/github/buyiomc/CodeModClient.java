caf2c9d30156e04bbb48ead0d1a2cb80233665e9
buyioMC <2679370735@qq.com>
2024-11-16 10:12:18 +0800

    Initial commit

---
+++ src/main/java/io/github/buyiomc/CodeModClient.java
@@ -0,0 +1,10 @@
+package io.github.buyiomc;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CodeModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/io/github/buyiomc/CodeModDataGenerator.java
@@ -0,0 +1,11 @@
+package io.github.buyiomc;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CodeModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
